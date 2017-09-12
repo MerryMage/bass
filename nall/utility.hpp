bd4962f866e2758f5a6a9d532e71d2d0da358995
MerryMage <MerryMage@users.noreply.github.com>
2017-09-12 20:36:00 +0100

    Update to v14r06 release.
    v14r06 was released on 2017-06-29. byuu said:
    
    Changelog:
    * `scope {` removed -- didn't appear to serve any purpose
    * `scope name {` is now `namespace name {`
    * `scope name: {` is now `function name {`
    * fixed `global` prefix ... it wasn't working at all before
    * arch snes.cpu: added tcs/tsc variants for tas/tsa
    * macros are now always scoped no matter what
    
    The thing with macros and scoping is ... they create their own stack frames for variables and such. Once the macro execution concludes, the frames are destroyed. Even if that weren't the case, macros support arity overriding, so you can have `test()` and `test(arg1, arg2)` and internally they become `test:0` and `test:2`. If you want a macro to create a variable outside of its execution stack frame, then you'll want to prefix the variable with `global` to do that.
    
    I updated the documentation with this WIP as well to reflect the changes.

--- nall/utility.hpp
+++ nall/utility.hpp
@@ -1,21 +1,27 @@
-#ifndef NALL_UTILITY_HPP
-#define NALL_UTILITY_HPP
+#pragma once
 
 #include <utility>
 
 namespace nall {
 
 template<typename T> struct base_from_member {
-  T value;
   base_from_member(T value) : value(value) {}
+  T value;
 };
 
-template<typename T> inline T* allocate(unsigned size, const T& value) {
+template<typename To, typename With> struct castable {
+  operator To&() { return (To&)value; }
+  operator const To&() const { return (const To&)value; }
+  operator With&() { return value; }
+  operator const With&() const { return value; }
+  auto& operator=(const With& value) { return this->value = value; }
+  With value;
+};
+
+template<typename T> inline auto allocate(uint size, const T& value) -> T* {
   T* array = new T[size];
-  for(unsigned i = 0; i < size; i++) array[i] = value;
+  for(uint i = 0; i < size; i++) array[i] = value;
   return array;
 }
 
 }
-
-#endif
